c97b55c3b227a308ee8553215ce42d072a32c5c0
Eduardo Arregui <er.arregui@gmail.com>
2019-02-19 12:26:50 -0300

    Base poejet 22

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+            <div class="row">
+                <div class="col-xs-8 col-xs-offset-2">
+                    <div id="app">
+                        <idea></idea>
+                        <tareas></tareas>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+@endsection
