2d25b79342cfa14d181e8da0eae4e82d54931ed7
Eduardo Arregui <er.arregui@gmail.com>
2019-04-12 15:36:12 -0300

    proyecto finised

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -13,12 +13,12 @@ class EmailController extends Controller
 {
    public function send(Request $request)
    {
-   	$name = $request->input('name');
+   	/*$name = $request->input('name');
    	$email = $request->input('email');
-   	$message = $request->input('message');
+   	$message = $request->input('message');*/
 
    	//dd($name, $email, $message);
    	$admins = User::where('admin', true)->get();
-    Mail::to($admins)->send(new NewMessage($name, $email, $message));
+    Mail::to($admins)->send(new NewMessage($request->name, $request->email, $request->message));
    }
 }
